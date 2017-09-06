3909ad93ed9105b684203fddeb9c26b5b51add01
wansu <wansu@segasoft.cn>
2017-09-06 20:07:24 +0800

    子页面可以向父页面传递参数

--- reactTest/my-app/src/components/BodyIndex.js
+++ reactTest/my-app/src/components/BodyIndex.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BodyChild from './bodychild'
 
 export default class BodyIndex extends React.Component {
 
     constructor() {
         super();//继承父类的
-        this.state = {username: "parry"};//初始化赋值
+        this.state = {username: "parry",age:20};//初始化赋值
+
+    }
+    handleChildValueChange(event){
+        this.setState({age:event.target.value});
 
     }
 
+    changeUserInfo(age){
+        this.setState({age:age});
+    }
 
     render() {
 
-        setTimeout(() => {
-            this.setState({username: "wansu"});
-        }, 4000)
+        // setTimeout(() => {
+        //     this.setState({username: "wansu"});
+        // }, 4000)
         return (
             <div>
                 <h2>页面的主体内容</h2>
-                <p>{this.state.username}  {this.props.userid} {this.props.username}</p>
+                <p>{this.state.age} {this.props.userid} {this.props.username}</p>
+                <input type="button" value="提交" onClick={this.changeUserInfo.bind(this,99) } />
+                <BodyChild handleChildValueChange={this.handleChildValueChange.bind(this)}/>
             </div>
         )
 
